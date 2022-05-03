38a4e2c2a6cd5a4970db7ae12e8b59708b26b597
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-05-02 23:01:18 -0400

    some validation for insertion

--- datapotato-insert-next-jdbc/src/donut/datapotato/insert/next_jdbc.clj
+++ datapotato-insert-next-jdbc/src/donut/datapotato/insert/next_jdbc.clj
@@ -7,23 +7,29 @@
 
 (defn perform-insert
   [{:keys [insert] :as ent-db}
-   {:keys [ent-name visit-val]}]
+   {:keys [ent-name ent-type visit-val]}]
   (let [{:keys [get-insert-db get-inserted]} insert
         get-inserted                         (or get-inserted (fn [{:keys [insert-result]}]
                                                                 insert-result))
         db                                   (get-insert-db)
-        table-name                           (get-in (dd/ent-schema ent-db ent-name) [visit-key :table-name])
-        insert-result                        (jsql/insert! db
-                                                           table-name
-                                                           visit-val)]
-    (->> (get-inserted {:db            db
-                        :table-name    table-name
-                        :insert-result insert-result})
-         (reduce-kv (fn [m k v]
-                      (assoc m
-                             (-> k name keyword)
-                             v))
-                    {}))))
+        table-name                           (get-in (dd/ent-schema ent-db ent-name) [visit-key :table-name])]
+
+    (when-not db
+      (throw (ex-info "get-insert-db did not return db" {:db db})))
+
+    (when-not table-name
+      (throw (ex-info "no table name provided" {:ent-name ent-name
+                                                :ent-type ent-type})))
+
+    (let [insert-result (jsql/insert! db table-name visit-val)]
+      (->> (get-inserted {:db            db
+                          :table-name    table-name
+                          :insert-result insert-result})
+           (reduce-kv (fn [m k v]
+                        (assoc m
+                               (-> k name keyword)
+                               v))
+                      {})))))
 
 (def insert-generated
   (dd/wrap-incremental-insert-visiting-fn :generate perform-insert))
