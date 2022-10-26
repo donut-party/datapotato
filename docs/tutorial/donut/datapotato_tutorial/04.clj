73dd98676fb878a941c439c5db2cc244db49703b
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-10-25 20:44:58 -0400

    more tutorial updates

--- docs/tutorial/donut/datapotato_tutorial/04.clj
+++ docs/tutorial/donut/datapotato_tutorial/04.clj
@@ -1,29 +1,34 @@
 (ns donut.datapotato-tutorial.04
-  (:require [donut.datapotato.core :as sm]))
+  (:require [donut.datapotato.core :as dc]))
 
-(def schema
+(def potato-schema
   {:user  {:prefix :u}
    :topic {:prefix    :t
            :relations {:owner-id [:user :id]}}
    :post  {:prefix    :p
            :relations {:topic-id [:topic :id]
                        :owner-id [:user :id]}}})
+
+(def potato-db
+  {:schema potato-schema})
+
 (defn ex-01
   []
-  (sm/add-ents {:schema schema} {:topic [[2 {:refs {:owner-id :my-own-sweet-user}}]
-                                         [1]]}))
+  (dc/add-ents potato-db {:topic [{:count 2
+                                   :refs  {:owner-id :my-own-sweet-user}}
+                                  {:count 1}]}))
 
-(sm/view (ex-02))
 (defn ex-02
   []
-  (sm/add-ents {:schema schema} {:topic [[1]
-                                         [1 {:refs {:owner-id :hamburglar}}]]
-                                 :post  [[1]
-                                         [1 {:refs {:topic-id :t1}}]]}))
+  (dc/add-ents potato-db {:topic [{:count 1}
+                                  {:refs  {:owner-id :hamburglar}}]
+                          :post  [{:count 1}
+                                  {:refs  {:topic-id :t1}}]}))
 
 (defn ex-03
   []
-  (sm/add-ents {:schema schema} {:topic [[:t0]
-                                         [:t1 {:refs {:owner-id :hamburglar}}]]
-                                 :post  [[1 {:refs {:topic :tl0}}]
-                                         [1 {:refs {:topic :tl1}}]]}))
+  (dc/add-ents potato-db {:topic [{:ent-name :t0}
+                                  {:ent-name :t1
+                                   :refs     {:owner-id :hamburglar}}]
+                          :post  [{:refs {:topic :tl0}}
+                                  {:refs {:topic :tl1}}]}))
