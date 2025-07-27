3a72fd3e61e96fabee745468496d963a79d53b17
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2025-07-27 14:28:05 -0400

    use the client api instead of the peer

--- datapotato-datomic/src/donut/datapotato/datomic.clj
+++ datapotato-datomic/src/donut/datapotato/datomic.clj
@@ -1,12 +1,12 @@
 (ns donut.datapotato.datomic
   (:require
    [donut.datapotato.core :as dc]
-   [datomic.api :as d]))
+   [datomic.client.api :as d]))
 
 (defn insert
   [{{:keys [connection]} dc/fixtures-visit-key}
    {:keys [visit-val]}]
-  (let [result @(d/transact connection [visit-val])
-        dbid (first (vals (:tempids result)))]
+  (let [result (d/transact connection {:tx-data [(assoc visit-val :db/id "tempid")]})
+        dbid   (first (vals (:tempids result)))]
     (assoc (d/pull (d/db connection) '[*] dbid)
            :db/id dbid)))
--- datapotato-datomic/test/donut/datapotato/datomic_test.clj
+++ datapotato-datomic/test/donut/datapotato/datomic_test.clj
@@ -1,10 +1,11 @@
 (ns donut.datapotato.datomic-test
   (:require
    [clojure.test :refer [deftest is testing]]
-   [datomic.api :as d]
+   [datomic.client.api :as d]
    [donut.datapotato.core :as dc]
    [donut.datapotato.datomic :as dd]
    [malli.generator :as mg]
+   [matcher-combinators.matchers :as mcm]
    [matcher-combinators.test]))
 
 
@@ -53,6 +54,16 @@
                :prefix      :p}})
 
 (def uri "datomic:mem://datapotato-test")
+(def client (d/client {:server-type :datomic-local
+                       :storage-dir :mem
+                       :system      "dev"}))
+
+(def db-arg-map {:db-name "datapotato-test"})
+
+(defn get-connection [_]
+  (d/delete-database client db-arg-map)
+  (d/create-database client db-arg-map)
+  (d/connect client db-arg-map))
 
 (def potato-db
   {:schema   schema
@@ -61,56 +72,40 @@
               dd/insert
 
               :get-connection
-              (fn get-connection [_]
-                (d/delete-database uri)
-                (d/create-database uri)
-                (d/connect uri))
+              get-connection
 
               :setup
               (fn setup [_]
-                @(d/transact
-                  dc/*connection*
-                  [{:db/ident              :user/username
-                    :db/id                 #db/id [:db.part/db]
-                    :db/valueType          :db.type/string
-                    :db/cardinality        :db.cardinality/one
-                    :db.install/_attribute :db.part/db}
+                (d/transact dc/*connection* {:tx-data
+                                             [{:db/ident       :user/username
+                                               :db/valueType   :db.type/string
+                                               :db/cardinality :db.cardinality/one}
 
-                   {:db/ident              :todo-list/created-by
-                    :db/id                 #db/id [:db.part/db]
-                    :db/valueType          :db.type/ref
-                    :db/cardinality        :db.cardinality/one
-                    :db.install/_attribute :db.part/db}
+                                              {:db/ident       :todo-list/created-by
+                                               :db/valueType   :db.type/ref
+                                               :db/cardinality :db.cardinality/one}
 
-                   {:db/ident              :todo/todo-title
-                    :db/id                 #db/id [:db.part/db]
-                    :db/valueType          :db.type/string
-                    :db/cardinality        :db.cardinality/one
-                    :db.install/_attribute :db.part/db}
+                                              {:db/ident       :todo/todo-title
+                                               :db/valueType   :db.type/string
+                                               :db/cardinality :db.cardinality/one}
 
-                   {:db/ident              :todo/created-by
-                    :db/id                 #db/id [:db.part/db]
-                    :db/valueType          :db.type/ref
-                    :db/cardinality        :db.cardinality/one
-                    :db.install/_attribute :db.part/db}
+                                              {:db/ident       :todo/created-by
+                                               :db/valueType   :db.type/ref
+                                               :db/cardinality :db.cardinality/one}
 
-                   {:db/ident              :todo/todo-list
-                    :db/id                 #db/id [:db.part/db]
-                    :db/valueType          :db.type/ref
-                    :db/cardinality        :db.cardinality/one
-                    :db.install/_attribute :db.part/db}
+                                              {:db/ident       :todo/todo-list
+                                               :db/valueType   :db.type/ref
+                                               :db/cardinality :db.cardinality/one}
 
-                   {:db/ident              :project/todo-lists
-                    :db/id                 #db/id [:db.part/db]
-                    :db/valueType          :db.type/ref
-                    :db/cardinality        :db.cardinality/many
-                    :db.install/_attribute :db.part/db}
+                                              {:db/ident       :project/todo-lists
+                                               :db/valueType   :db.type/ref
+                                               :db/cardinality :db.cardinality/many}
 
-                   {:db/ident              :project/created-by
-                    :db/id                 #db/id [:db.part/db]
-                    :db/valueType          :db.type/ref
-                    :db/cardinality        :db.cardinality/one
-                    :db.install/_attribute :db.part/db}]))}})
+                                              {:db/ident       :project/created-by
+                                               :db/valueType   :db.type/ref
+                                               :db/cardinality :db.cardinality/one}]}))}})
+
+(defn expected-ref [m] (select-keys m [:db/id]))
 
 ;;---
 ;; tests
@@ -125,65 +120,68 @@
 
 (deftest inserts-simple-generated-data
   (dc/with-fixtures potato-db
-    (dc/insert-fixtures {:user [{:count 2}]})
-    (is (match? [{:db/id 17592186045418 :user/username string?}
-                 {:db/id 17592186045420 :user/username string?}]
-                (q dc/*connection*
-                   '{:find  [(pull ?u [*])]
-                     :where [[?u :user/username]]})))))
+    (let [{:keys [u0 u1]} (dc/insert-fixtures {:user [{:count 2}]})]
+      (is (match? [{:db/id pos-int? :user/username string?}
+                   {:db/id pos-int? :user/username string?}]
+                  [u0 u1]))
+      (is (= #{u0 u1}
+             (set (q dc/*connection*
+                     '{:find  [(pull ?u [*])]
+                       :where [[?u :user/username]]})))))))
 
 
 (deftest inserts-generated-data-hierarchy
   (dc/with-fixtures potato-db
-    (dc/insert-fixtures {:todo [{:count 2}]})
-    (is (match? [{:db/id 17592186045418
-                  :user/username string?}]
-                (q dc/*connection*
-                   '{:find  [(pull ?u [*])]
-                     :where [[?u :user/username]]})))
+    (let [{:keys [u0 tl0]} (dc/insert-fixtures {:todo [{:count 2}]})
+          expected-u       (expected-ref u0)
+          expected-tl      (expected-ref tl0)]
+      (is (match? [{:db/id         pos-int?
+                    :user/username string?}]
+                  (q dc/*connection*
+                     '{:find  [(pull ?u [*])]
+                       :where [[?u :user/username]]})))
 
-    (is (match? [{:db/id           17592186045422
-                  :todo/todo-title "write unit tests"
-                  :todo/created-by #:db{:id 17592186045418}
-                  :todo/todo-list  #:db{:id 17592186045420}}
-                 {:db/id           17592186045424
-                  :todo/todo-title "write unit tests"
-                  :todo/created-by #:db{:id 17592186045418}
-                  :todo/todo-list  #:db{:id 17592186045420}}]
-                (q dc/*connection*
-                   '{:find  [(pull ?u [*])]
-                     :where [[?u :todo/todo-title]]})))
+      (is (match? [{:db/id           pos-int?
+                    :todo/todo-title "write unit tests"
+                    :todo/created-by expected-u
+                    :todo/todo-list  expected-tl}
+                   {:db/id           pos-int?
+                    :todo/todo-title "write unit tests"
+                    :todo/created-by expected-u
+                    :todo/todo-list  expected-tl}]
+                  (q dc/*connection*
+                     '{:find  [(pull ?u [*])]
+                       :where [[?u :todo/todo-title]]})))
 
-    (is (match? [{:db/id 17592186045420,
-                  :todo-list/created-by #:db{:id 17592186045418}}]
-                (q dc/*connection*
-                   '{:find  [(pull ?u [*])]
-                     :where [[?u :todo-list/created-by]]})))))
+      (is (match? [{:db/id                pos-int?
+                    :todo-list/created-by expected-u}]
+                  (q dc/*connection*
+                     '{:find  [(pull ?u [*])]
+                       :where [[?u :todo-list/created-by]]}))))))
 
 (deftest inserts-colls
   (dc/with-fixtures potato-db
-    (dc/insert-fixtures {:project [{:refs {:project/todo-lists 2}}]})
+    (let [{:keys [u0 tl0 tl1]} (dc/insert-fixtures {:project [{:refs {:project/todo-lists 2}}]})
+          expected-u           (expected-ref u0)
+          expected-tl0         (expected-ref tl0)
+          expected-tl1         (expected-ref tl1)]
 
-    (is (match? [{:db/id         17592186045418
-                  :user/username string?}]
-                (q dc/*connection*
-                   '{:find  [(pull ?u [*])]
-                     :where [[?u :user/username]]})))
+      (is (= [u0]
+             (q dc/*connection*
+                '{:find  [(pull ?u [*])]
+                  :where [[?u :user/username]]})))
 
-    (is (match? [{:db/id                17592186045420,
-                  :todo-list/created-by #:db{:id 17592186045418}}
-                 {:db/id                17592186045422,
-                  :todo-list/created-by #:db{:id 17592186045418}}]
-                (q dc/*connection*
-                   '{:find  [(pull ?u [*])]
-                     :where [[?u :todo-list/created-by]]})))
+      (is (= #{tl0 tl1}
+             (set (q dc/*connection*
+                     '{:find  [(pull ?u [*])]
+                       :where [[?u :todo-list/created-by]]}))))
 
-    (is (match? [{:db/id              17592186045424,
-                  :project/todo-lists [{:db/id 17592186045420} {:db/id 17592186045422}]
-                  :project/created-by {:db/id 17592186045418}}]
-                (q dc/*connection*
-                   '{:find  [(pull ?u [*])]
-                     :where [[?u :project/created-by]]})))))
+      (is (match? [{:db/id              pos-int?
+                    :project/todo-lists (mcm/in-any-order [expected-tl0 expected-tl1])
+                    :project/created-by expected-u}]
+                  (q dc/*connection*
+                     '{:find  [(pull ?u [*])]
+                       :where [[?u :project/created-by]]}))))))
 
 ;;---
 ;; polymorphic test
@@ -220,89 +218,69 @@
               dd/insert
 
               :get-connection
-              (fn get-connection [_]
-                (d/delete-database uri)
-                (d/create-database uri)
-                (d/connect uri))
+              get-connection
 
               :setup
               (fn setup [{:keys [fixtures]}]
                 (let [{:keys [connection]} fixtures]
-                  @(d/transact
-                    connection
-                    [{:db/ident              :topic-category/name
-                      :db/id                 #db/id [:db.part/db]
-                      :db/valueType          :db.type/string
-                      :db/cardinality        :db.cardinality/one
-                      :db.install/_attribute :db.part/db}
+                  (d/transact connection {:tx-data [{:db/ident       :topic-category/name
+                                                     :db/valueType   :db.type/string
+                                                     :db/cardinality :db.cardinality/one}
 
-                     {:db/ident              :topic/topic-category
-                      :db/id                 #db/id [:db.part/db]
-                      :db/valueType          :db.type/ref
-                      :db/cardinality        :db.cardinality/one
-                      :db.install/_attribute :db.part/db}
+                                                    {:db/ident       :topic/topic-category
+                                                     :db/valueType   :db.type/ref
+                                                     :db/cardinality :db.cardinality/one}
 
-                     {:db/ident              :watch/watched
-                      :db/id                 #db/id [:db.part/db]
-                      :db/valueType          :db.type/ref
-                      :db/cardinality        :db.cardinality/one
-                      :db.install/_attribute :db.part/db}])))}})
+                                                    {:db/ident       :watch/watched
+                                                     :db/valueType   :db.type/ref
+                                                     :db/cardinality :db.cardinality/one}]})))}})
 
 
 (deftest polymorphic-refs
   (testing "incremental insert two different polymorphic refs"
     (dc/with-fixtures polymorphic-potato-db
-      (let [updated-db (dc/insert-fixtures {:watch [{:count     1
-                                                     :ref-types {:watch/watched :topic-category}}]})]
+      (let [{:keys [w0 tc0] :as updated-db} (dc/insert-fixtures {:watch [{:count     1
+                                                                          :ref-types {:watch/watched :topic-category}}]})]
 
-        (is (match? [{:db/id               17592186045418
-                      :topic-category/name "topic category"}]
+        (is (match? [tc0]
                     (q dc/*connection*
                        '{:find  [(pull ?e [*])]
                          :where [[?e :topic-category/name]]})))
 
-        (is (match? [{:db/id         17592186045420
-                      :watch/watched {:db/id 17592186045418}}]
+        (is (match? [w0]
                     (q dc/*connection*
                        '{:find  [(pull ?e [*])]
                          :where [[?e :watch/watched]]})))
 
-        (dc/insert-fixtures updated-db {:watch [{:count     1
-                                                 :ref-types {:watch/watched :topic}}]})
+        (let [{:keys [w1 t0]} (dc/insert-fixtures updated-db {:watch [{:count     1
+                                                                       :ref-types {:watch/watched :topic}}]})]
 
-        (is (match? [{:db/id                17592186045422
-                      :topic/topic-category {:db/id 17592186045418}}]
-                    (q dc/*connection*
-                       '{:find  [(pull ?e [*])]
-                         :where [[?e :topic/topic-category]]})))
+          (is (match? [t0]
+                      (q dc/*connection*
+                         '{:find  [(pull ?e [*])]
+                           :where [[?e :topic/topic-category]]})))
 
-        (is (match? [{:db/id         17592186045420
-                      :watch/watched {:db/id 17592186045418}}
-                     {:db/id         17592186045424
-                      :watch/watched {:db/id 17592186045422}}]
-                    (q dc/*connection*
-                       '{:find  [(pull ?e [*])]
-                         :where [[?e :watch/watched]]}))))))
+          (is (= (set [w0 w1])
+                 (set (q dc/*connection*
+                         '{:find  [(pull ?e [*])]
+                           :where [[?e :watch/watched]]}))))))))
 
   (testing "single polymorphic ref"
     (dc/with-fixtures polymorphic-potato-db
-      (dc/insert-fixtures {:watch [{:count     1
-                                    :ref-types {:watch/watched :topic}}]})
+      (let [{:keys [t0 tc0 w0]} (dc/insert-fixtures {:watch [{:count     1
+                                                              :ref-types {:watch/watched :topic}}]})]
 
-      (is (match? [{:db/id               17592186045418
-                    :topic-category/name "topic category"}]
-                  (q dc/*connection*
-                     '{:find  [(pull ?e [*])]
-                       :where [[?e :topic-category/name]]})))
+        (is (= [tc0]
+               (q dc/*connection*
+                  '{:find  [(pull ?e [*])]
+                    :where [[?e :topic-category/name]]})))
 
-      (is (match? [{:db/id                17592186045420
-                    :topic/topic-category {:db/id 17592186045418}}]
-                  (q dc/*connection*
-                     '{:find  [(pull ?e [*])]
-                       :where [[?e :topic/topic-category]]})))
+        (is (= [t0]
+               (q dc/*connection*
+                  '{:find  [(pull ?e [*])]
+                    :where [[?e :topic/topic-category]]})))
 
-      (is (match? [{:db/id         17592186045422
-                    :watch/watched {:db/id 17592186045420}}]
-                  (q dc/*connection*
-                     '{:find  [(pull ?e [*])]
-                       :where [[?e :watch/watched]]}))))))
+        (is (= [w0]
+               (q dc/*connection*
+                  '{:find  [(pull ?e [*])]
+                    :where [[?e :watch/watched]]})))))))
